312ae1b88e3e649ee61ab0fc14c6ef8880967c21
sigfried <rssanjuan2704@gmail.com>
2020-03-29 12:39:58 -0600

    refactor(feat): Add react-query to handle caching, async task completion and other

--- src/components/ResultsComponent/ResultsComponent.jsx
+++ src/components/ResultsComponent/ResultsComponent.jsx
@@ -1,32 +1,14 @@
-import React from 'react'
+import { useQuery } from 'react-query'
 
 import IssuesApiService from '../../services/issues-api'
 
 const ResultsComponent = ({ searchTerm, currentPage = 1, children }) => {
-  const [results, setResults] = React.useState({ items: [] })
-  const [loading, setLoading] = React.useState(true)
-  const [error, setError] = React.useState('')
+  const { status, data, error } = useQuery(
+    searchTerm && ['issues', searchTerm, currentPage],
+    IssuesApiService.getIssues,
+  )
 
-  React.useEffect(() => {
-    if (searchTerm) {
-      const search = async () => {
-        try {
-          setLoading(true)
-          const results = await IssuesApiService.getIssues(
-            searchTerm,
-            currentPage,
-          )
-          setResults(results)
-          setLoading(false)
-        } catch (err) {
-          setError(err.message)
-        }
-      }
-      search(searchTerm)
-    }
-  }, [searchTerm, currentPage])
-
-  return results ? children({ data: results, error, loading }) : null
+  return children({ data, error, status })
 }
 
 export default ResultsComponent
