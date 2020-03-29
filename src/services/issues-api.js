9229d332e930d76fc7efbae98400e540304f54fe
sigfried <rssanjuan2704@gmail.com>
2020-03-29 12:37:17 -0600

    refactor(doc): Add docs for getIssues method

--- src/services/issues-api.js
+++ src/services/issues-api.js
@@ -1,13 +1,22 @@
 import { getRequest } from './fetch-api'
 
 class IssuesApiService {
-  static async getIssues(searchTerm, currentPage) {
+  /**
+   * Get issues from facebook/react repository
+   * @param {String} key - This key belongs to react-query
+   * @param {String} searchTerm - Search time to search inside repo's issues
+   * @param {Number} currentPage - Current page in order to be able to paginate
+   *
+   * @return {Promise}
+   *
+   */
+  static async getIssues(key, searchTerm, currentPage) {
     try {
       return await getRequest('issues', {
         searchParams: `?q=${searchTerm}+repo:facebook/react&page=${currentPage}`,
       }).json()
     } catch (err) {
-      console.log(err)
+      throw new Error(err.message)
     }
   }
 }
