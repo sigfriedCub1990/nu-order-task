dbe66ee8e8ee7fe1c3159a248db4ad78d430969b
sigfried <rssanjuan2704@gmail.com>
2020-03-29 12:36:30 -0600

    feat: Add simple test for main App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders search an issue label', () => {
+  const { getByText } = render(<App />)
+  const label = getByText(/Search an issue/i)
+  expect(label).toBeInTheDocument()
+})
