44437a6c931f6801c4180f2520fe87de5f2a400f
sigfried <rssanjuan2704@gmail.com>
2020-03-29 12:48:37 -0600

    fix: Constants imports

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,3 @@
-const API_ROOT_PATH = 'https://api.github.com/search'
+export const API_ROOT_PATH = 'https://api.github.com/search'
 
-const DEBOUNCE_DELAY = 500
-
-export default { API_ROOT_PATH, DEBOUNCE_DELAY }
+export const DEBOUNCE_DELAY = 500
