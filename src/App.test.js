c66f97b56355b74f0e99fa9615cd1fcdafc87c34
sigfried <rssanjuan2704@gmail.com>
2020-03-29 17:38:05 -0600

    fix: Failing App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, wait, screen } from '@testing-library/react'
 import App from './App'
 
-test('renders search an issue label', () => {
-  const { getByText } = render(<App />)
-  const label = getByText(/Search an issue/i)
-  expect(label).toBeInTheDocument()
+test('Renders main app', async () => {
+  render(<App />)
+
+  await wait(() => screen.getByText(/Search an issue/i))
+
+  expect(screen.getByText(/Search an issue/i)).toBeInTheDocument()
 })
